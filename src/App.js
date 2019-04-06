3bc2201510437e5517c014a053845c683233811e
Richard Hung <richardhung111@gmail.com>
2019-04-06 13:29:20 -0700

    randomize start and append chart data

--- src/App.js
+++ src/App.js
@@ -7,8 +7,44 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      runningData: data,
+      startIndex: 0,
+      counter: 0,
+      limit: 120,
+      runningData: [],
     };
+  }
+  componentDidMount() {
+    this.initGame();
+  }
+  addPointToRunningData() {
+    let counter = this.state.counter + 1;
+    let sliceStart = this.state.startIndex;
+    let sliceEnd = this.state.startIndex + counter;
+    let runningData = data.slice(sliceStart, sliceEnd);
+
+    this.setState({
+      counter: counter,
+      runningData: runningData,
+    });
+  }
+  initGame() {
+    // Randomize start index
+    let count = data.length;
+    let maxStart = count - this.state.limit;
+    let startIndex = Math.floor(Math.random() * Math.floor(maxStart));
+
+    // Reset variables
+    this.setState({
+      counter: 0,
+      runningData: [],
+      startIndex: startIndex,
+    });
+
+    this.interval = setInterval(() => {
+      this.addPointToRunningData();
+
+      if (this.state.counter > this.state.limit) clearInterval(this.interval);
+    }, 100);
   }
   render() {
     return (
